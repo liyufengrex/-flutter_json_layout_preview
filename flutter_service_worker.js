79b847d571679f2e332c75b755928b2f89902e6c
liyufeng1 <liyufeng1@pin-dao.cn>
2023-06-05 15:39:56 +0800

    init

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "ff966ab969ba381b900e61629bfb9789",
-"index.html": "d19e459c57ddb9e1f0127b37b5ce7967",
-"/": "d19e459c57ddb9e1f0127b37b5ce7967",
-"main.dart.js": "fe5d81688d46b70bfdb945dbb1be6787",
+"index.html": "ddf7d3627ab215d8bd3c7a6e03174cd5",
+"/": "ddf7d3627ab215d8bd3c7a6e03174cd5",
+"main.dart.js": "b009a4bfc4d1d42fb9b65fe737c8fcc8",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
